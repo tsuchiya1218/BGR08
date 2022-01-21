a8993599b6c78a4c388927ba3bfc16bbf4670d85
20jy0208 <20jy0208@jec.ac.jp>
2022-01-21 13:03:41 +0900

    kadai14

---
+++ addcart_complete.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<link rel="stylesheet" href="./css/center.css" type="text/css">
+		<link rel="stylesheet" href="./css/fontsize.css" type="text/css">
+		<meta charset="UTF-8">
+	</head>
+	<body class="center">
+        <main>
+    		<a herf="top.php"></a>
+	    	<a herf="cart.php"></a>
+		    <h2><p class="title">追加完了</p></h2>
+		    <p>カート追加完了しました</p>
+            <input type="button" onClick="location.href='cart.php'" value="カート">
+            <input type="button" onClick="location.href='software.php'" value="商品検索画面">
+        </main>
+	</body>
+</html>

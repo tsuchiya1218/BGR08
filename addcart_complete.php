4aa1ce71f3b78fcd17474fa57a1d32bac8b68c11
20jy0208 <20jy0208@jec.ac.jp>
2022-01-21 13:05:53 +0900

    kadai14

--- addcart_complete.php
+++ addcart_complete.php
@@ -1,11 +1,24 @@
 <!DOCTYPE html>
-<html>
+	<html lang="ja">
 	<head>
-		<link rel="stylesheet" href="./css/center.css" type="text/css">
-		<link rel="stylesheet" href="./css/fontsize.css" type="text/css">
 		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Jtendoトップ画面</title>
+		<link href="./css/top.css" rel="stylesheet" type="text/css">
+		<link href="./css/common.css" rel="stylesheet" type="text/css">
 	</head>
-	<body class="center">
+	<body>
+		<header>
+			<nav>
+				<ul class="gnav-list-1">
+					<li><a href="top.php">Top<br>トップ</a></li>
+					<li><a href="software.php">Software<br>ソフト</a></li>
+					<li><a href="support.php">Support<br>サポート</a></li>
+					<li><a href="cart.php">Cart<br>カート</a></li>
+                    <li><a href="order_history.php">OrderHistory<br>注文履歴</a></li>
+				</ul>
+			</nav>
+		</header>
         <main>
     		<a herf="top.php"></a>
 	    	<a herf="cart.php"></a>
@@ -14,5 +27,9 @@
             <input type="button" onClick="location.href='cart.php'" value="カート">
             <input type="button" onClick="location.href='software.php'" value="商品検索画面">
         </main>
+		<footer>
+				<p class="footerline"></p>
+				<p><small>&copy;Copyright Jtendo. All rights reserved.</small><p>
+		</footer>
 	</body>
 </html>
